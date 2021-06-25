247ff47e7cb9a8448a64fe6720f6250fc689df2e
Chinapanda <kittikornchina2014@gmail.com>
2021-06-25 15:01:08 +0700

    RXJS with asynchronous

--- Observable/index.js
+++ Observable/index.js
@@ -5,11 +5,29 @@ const observer = {
   error: (error) => console.log("error", error),
   complete: () => console.log("complete"),
 };
+// const observable = new Observable((subscribe) => {
+//   subscribe.next("Hello");
+//   subscribe.next("World");
+//   subscribe.complete();
+//   //cannot subscribe more after  complete
+// });
+
 const observable = new Observable((subscribe) => {
-  subscribe.next("Hello");
-  subscribe.next("World");
-  subscribe.complete();
-  //cannot subscribe more after  complete
+  let count = 0;
+  const id = setInterval(() => {
+    subscribe.next(count);
+    if (count == 10) {
+      subscribe.complete();
+    }
+    // subscribe.complete();
+    count += 1;
+  }, 1000);
+    return () => {
+      console.log("called");
+      clearInterval(id);
+    };
 });
 
+console.log("before");
 observable.subscribe(observer);
+console.log("after");
